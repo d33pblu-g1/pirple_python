9b643378d8d7293912a5366e0c3762a6d3c8ef0e
d33pblu-g1 <76946721+d33pblu-g1@users.noreply.github.com>
2021-01-05 15:59:09 +0200

    add main.py and gitignore

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+printing the 
+Favourite band program
+"""
+
+# Setting Variables
+Band = "Linkin Park"
+Genre = "Rock"
+Formed = "1996"
+Vocalist = "Mike Shinoda"
+Guitarist = "Brad Delson"
+Bassist = "Dave Farrell"
+DJ = "Joe Hahn"
+Drummer = "Rob Bourdon"
+
+# printing the variables
+print (Band)
+print (Genre)
+print (Formed)
+print (Vocalist)
+print (Guitarist)
+print (Bassist)
+print (DJ)
+print (Drummer)
